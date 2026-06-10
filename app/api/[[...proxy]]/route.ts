2f7fde9386c1d4abfba8fdc67e95d600a96b04a0
Dhanush <ramudridhanush@gmail.com>
2026-06-10 23:48:20 +0530

    migrate: render to aws

--- app/api/[[...proxy]]/route.ts
+++ app/api/[[...proxy]]/route.ts
@@ -4,26 +4,34 @@ async function proxyHandler(request: NextRequest) {
   try {
     const url = new URL(request.url);
     const pathname = url.pathname;
-    const searchParams = url.search; 
+    const searchParams = url.search;
     
     const apiPath = pathname.replace(/^\/api/, "");
-    
     const ec2Url = process.env.EC2_API_URL || "http://localhost:8000/api/v1";
-    const fullUrl = `${ec2Url}${apiPath}${searchParams}`; 
+    const fullUrl = `${ec2Url}${apiPath}${searchParams}`;
 
     console.log(`[Proxy] ${request.method} ${apiPath}${searchParams} -> ${fullUrl}`);
 
+    const headers = new Headers();
+    
+    if (request.headers.get("authorization")) {
+      headers.set("authorization", request.headers.get("authorization")!);
+    }
+
+    const contentType = request.headers.get("content-type");
+    if (contentType) {
+      headers.set("Content-Type", contentType);
+    }
+
+    let body: BodyInit | undefined;
+    if (request.method !== "GET" && request.method !== "HEAD") {
+      body = await request.arrayBuffer();
+    }
+
     const response = await fetch(fullUrl, {
       method: request.method,
-      headers: {
-        "Content-Type": "application/json",
-        ...(request.headers.get("authorization") && {
-          authorization: request.headers.get("authorization")!,
-        }),
-      },
-      body: request.method !== "GET" && request.method !== "HEAD" 
-        ? await request.text() 
-        : undefined,
+      headers: Object.fromEntries(headers),
+      body: body ? body : undefined,
     });
 
     const data = await response.json();
